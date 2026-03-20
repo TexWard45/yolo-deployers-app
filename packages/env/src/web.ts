0937716cbd9975d9e0821a694d6e37199daf40bb
Duc Ng <danielbaker06072001@gmail.com>
2026-03-20 22:22:03 +0700

    feat(env): add env

--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -1,9 +1,11 @@
 import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
 import { NodeEnvSchema } from "./shared";
 
 export const webEnv = createEnv({
   server: {
     NODE_ENV: NodeEnvSchema,
+    DATABASE_URL: z.string().url(),
   },
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
